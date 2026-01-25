932a67e93a6ceaca0a9fd1d673c46d4897c60d2d
Jagadeesh <jagadeeshmenda2005@gmail.com>
2026-01-25 21:38:07 +0530

    Coding_practice

---
+++ Emailcheck.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Emailcheck {
+
+    public static String emailcheck(String mail){
+        if("<>,{}%/".indexOf(mail)!=-1){
+            return "invalid email";
+        }
+        else{
+            for(int i=0;i<mail.length();i++){
+                if(mail.charAt(i)=='@'){
+                   return "Valid email domain name -"+ mail.substring(i);//returns domain name
+                }
+            }
+            return "invalid email";
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter email :");
+        String email=sc.next();
+         String result=emailcheck(email);
+         sc.close();
+         System.out.println(result);
+    }
+}
